37d084c892d2abbd9f289b3be62d299e6b8c6a7f
brob <bryanlrobinson@gmail.com>
2025-10-30 14:12:59 -0400

    Working with test data

--- src/pages/api/payment_success.js
+++ src/pages/api/payment_success.js
@@ -1,6 +1,7 @@
 export const prerender = false
 import {loadStripe} from '@stripe/stripe-js';
 import {createClient} from '@sanity/client'
+import { sanityClient } from 'sanity:client';
 
 const client = createClient({
     projectId: "4v8ldsm8",
@@ -9,33 +10,29 @@ const client = createClient({
     apiVersion: '2025-02-06', // use current date (YYYY-MM-DD) to target the latest API version. Note: this should always be hard coded. Setting API version based on a dynamic value (e.g. new Date()) may break your application at a random point in the future.
     token: import.meta.env.SANITY_WRITE // Needed for certain operations like updating content, accessing drafts or using draft perspectives
   })
-const stripe = await loadStripe(import.meta.env.STRIPE_SECRET);
 
 
 export async function POST({ request }) {
-    let event = request.body;
+  const data = (await request.json()).object
+  console.log(data)
 
-    const signature = request.headers['stripe-signature'];
-    try {
-      event = stripe.webhooks.constructEvent(
-        request.body,
-        signature,
-        endpointSecret
-      );
-    } catch (err) {
-      console.log(`⚠️  Webhook signature verification failed.`, err.message);
-      return response.sendStatus(400);
-    }
-console.log({event})
-    if (!event.metadata.sanity_id) return new Response({status: 500})
+    if (!data?.metadata?.sanity_id) return new Response('No metadata',{status: 500})
     
-    console.log(event.metadata)
+      try {
+        const response = await client.patch(data.metadata.sanity_id).dec({quantity: 1}).commit()
+        console.log(response)
+        return new Response(JSON.stringify({'name':'hi'}), {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
 
-    return new Response(JSON.stringify({'name':'hi'}), {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+      } catch(error) {
+        console.log(error)
+        return new Response(error,{status: 500})
+      }
+
+    
 }
   
